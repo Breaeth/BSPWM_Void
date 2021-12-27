2de29115a6e0dd07d29bb9312b83eff342fc55ba
Breaeth <68069297+Breaeth@users.noreply.github.com>
2021-12-27 13:23:10 +0100

    Add files via upload

---
+++ postinstall.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+### DO NOT LAUNCH AS ROOT ###
+
+if [ ! "$EUID" -ne 0 ]
+  then echo "Please do not run as root"
+       echo "chmod +x postinstall.sh and run it as your user"
+  exit
+fi
+
+### INSTALLING PACKAGE ###
+AUDIO="pipewire pulsemixer"
+WM="bspwm sxhkd"
+BASE="xorg-minimal xorg-font xf86-video-intel"
+BROWSER="qutebrowser"
+HUD="rofi polybar dunst lxappearance"
+TERMAPP="alacritty fish htop"
+
+sudo xbps-install $BASE $WM $AUDIO $HUD $BROWSER $TERMAPP
+
+### ENABLE APP'S FOR RUNIT ###
+
+sudo ln -s /etc/sv/wpa_supplicant /var/service
+sudo ln -s /etc/sv/dbus /var/service
+sudo ln -s /etc/sv/sddm /var/service
+
+### COPIYNG DOT FILES ###
+
+sudo cp -r /home/.* ~/
+
+### KEYBOARD LANGUAGE FOR X11 ###
+
+sudo mkdir -p /etc/X11/xorg.conf.d/
+sudo cp 00-keyboard.conf /etc/X11/xorg.conf.d/
+
+### GRUB CONFIGURATION ###
+
+sudo cp grub /etc/default/grub
