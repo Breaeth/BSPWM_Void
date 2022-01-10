db8f804943a45fdb5d2a24848e26e8dc3a5c7236
Breaeth <68069297+Breaeth@users.noreply.github.com>
2022-01-10 10:55:37 +0100

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -11,55 +11,25 @@ if [ "$EUID" -ne 0 ]
 fi
 
 ### INSTALLING PACKAGE ###
-AUDIO="pipewire pulsemixer alsa-pipewire alsa-utils pamixer"
-SPOTIFY="spotifyd spotify-qt"
-FONT="font-iosevka font-awesome5"
+AUDIO="pulseaudio pulsemixer"
 WM="bspwm sxhkd"
-BASE="xorg-minimal xorg-fonts xf86-video-intel sddm"
+BASE="xorg-minimal xorg-fonts xf86-video-intel"
 BROWSER="qutebrowser"
-HUD="rofi feh picom polybar dunst lxappearance"
-TERMAPP="alacritty neovim fish-shell htop neofetch wget maim setxkbmap xclip gpick"
-QT="qt5-svg qt5-quickcontrols2 qt5-graphicaleffects"
+HUD="rofi feh polybar dunst"
+TERMAPP="kitty neovim htop neofetch wget maim setxkbmap xclip"
 
-sudo xbps-install $BASE $WM $AUDIO $HUD $BROWSER $TERMAPP $SPOTIFY $FONT $QT
+sudo xbps-install $BASE $WM $AUDIO $HUD $BROWSER $TERMAPP
 
-### INSTALL PICOM-IBHAGWAN ###
-
-git clone --depth=1 https://github.com/void-linux/void-packages
-cd void-packages
-./xbps-src binary-bootstrap
-echo XBPS_ALLOW_RESTRICTED=yes >> etc/conf
-git clone https://github.com/ibhagwan/picom-ibhagwan-template
-mv picom-ibhagwan-template ./srcpkgs/picom-ibhagwan
-./xbps-src pkg picom-ibhagwan
-sudo xbps-install --repository=hostdir/binpkgs picom-ibhagwan 
-
-### CREATE IMPORTANT FOLDER ### 
-
-sudo mkdir -p /etc/sddm.conf.d
 
 ### ENABLE APP'S FOR RUNIT ###
 
 sudo ln -s /etc/sv/wpa_supplicant /var/service
 sudo ln -s /etc/sv/dbus /var/service
-sudo ln -s /etc/sv/sddm /var/service
 
-### GRUB theme ### 
-
-wget -O - https://github.com/shvchk/poly-dark/raw/master/install.sh | bash
 
 ### Discord ###
 
 wget https://dl.discordapp.net/apps/linux/0.0.16/discord-0.0.16.tar.gz | tar -xf 
 cd Discord ; sudo chmod +x Discord ; ./Discord
-
-### MOOVING IMPORTANT FILES ###
-
-cd $DIR
-mv .config .xinitrc .bash_profile $HOME
-#For SDDM
-mv sddm/sugar-candy /usr/share/sddm/themes/
-mv avatar.conf /etc/sddm.conf.d/
-#
 
 
